106c9fd5576db61298f626b42fd4baac0e30c5e3
ak66666 <32971450+ak66666@users.noreply.github.com>
2023-10-31 16:10:54 -0400

    Added VIA support to v5 in the new cleaned-up branch

---
+++ keyboards/thumbsup/rev5_promicro_unibody_5x12/config.h
@@ -0,0 +1,43 @@
+// Copyright 2023 ak66666 (@ak66666)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+
+#define USB_POLLING_INTERVAL_MS 1
+
+#define POINTING_DEVICE_ROTATION_90
+#define CIRQUE_PINNACLE_TAP_ENABLE
+// Limits the frequency that the sensor is polled for motion.
+#define POINTING_DEVICE_TASK_THROTTLE_MS 50  //10
+//#define CIRQUE_PINNACLE_SECONDARY_TAP_ENABLE
+
+// Circular scroll
+// Isn't it making the cursor jittering?
+#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
+
+//#define CIRQUE_PINNACLE_POSITION_MODE CIRQUE_PINNACLE_RELATIVE_MODE
+#define CIRQUE_PINNACLE_POSITION_MODE CIRQUE_PINNACLE_ABSOLUTE_MODE
+
+
+//The number of layers for VIA/REMAP
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+
+//This disables the auto-repeat for the double-action keys (e.g. S+LAlt)
+#define QUICK_TAP_TERM 0
